87863a67a35384d48d5758c115fcc7f994a86c60
luismarod <luismarod1408@gmail.com>
2021-03-04 11:58:18 -0300

    subiendo a heroku

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+require('./config/config');
+
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+
+
+io.on('connection', function (socket) {
+  console.log('Usuario conectado')
+
+  socket.broadcast.emit("newConnection", "Se ha unido una persona");
+
+  socket.on('pause', (data) => {
+    socket.broadcast.emit("pause", "pausa desde server");
+  });
+
+  socket.on('play', (data) => {
+    socket.broadcast.emit("play", "play desde server");
+  });
+
+  socket.on('timeupdate', ({ time }) => {
+    socket.broadcast.emit("timeupdate", { time });
+  });
+});
+
+
+
+
+server.listen(process.env.PORT, () => {
+  console.log(`Corriendo en puerto ${process.env.PORT}`)
+})
